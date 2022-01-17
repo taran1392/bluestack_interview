a1437cd67d03964b258656fca8f616467ff91e8f
Tarandeep Singh <tarandeep@ensureder.com>
2022-01-17 19:45:14 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    let timer = setInterval(() => setDate(new Date()), 1000)
+    return () => {
+      clearInterval(timer);
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div style={{textAlign: 'center'}}>
+        <h2>{date.toDateString()}</h2>
+        <h2>{date.toTimeString()}</h2>
+      </div>
     </div>
   );
 }
